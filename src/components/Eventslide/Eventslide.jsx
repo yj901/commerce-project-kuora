8a581865b927d275d4e3dabe3e09d91401a38c38
yj901 <901duwls@gmail.com>
2025-04-09 02:15:37 +0900

    eventslide-product-in

--- src/components/Eventslide/Eventslide.jsx
+++ src/components/Eventslide/Eventslide.jsx
@@ -1,16 +1,34 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useEffect, useState } from "react";
 import EventPoint from "./EventPoint";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Eventslide.scss";
 import EventSliderBtn from "../../assets/icons/event_slide_arr.svg";
+import { useProducts } from "../../contexts/ProductContext";
 
 const EventSlidePaging = ["serenity", "embrace", "momentum"];
 
 const EventSlide = () => {
+  const [eliasProducts, setEliasProducts] = useState([]);
+  const [activeCode, setActiveCode] = useState(null);
   const slickRef = useRef(null);
   const slideContainerRef = useRef(null);
+  const { allProducts } = useProducts();
+
+  useEffect(() => {
+    const filtered = Object.values(allProducts)
+      .flat()
+      .filter((product) => product.info?.collection === "Elias Klemens");
+    setEliasProducts(filtered);
+  }, [allProducts]);
+
+  const findCode = (code) => {
+    const found = eliasProducts.find((product) => {
+      return product.info.code === code;
+    });
+    return found;
+  };
 
   var settings = {
     dots: true,
@@ -50,8 +68,14 @@ const EventSlide = () => {
     ],
   };
 
-  const previous = useCallback(() => slickRef.current.slickPrev(), []);
-  const next = useCallback(() => slickRef.current.slickNext(), []);
+  const previous = useCallback(() => {
+    setActiveCode(null);
+    return slickRef.current.slickPrev();
+  }, []);
+  const next = useCallback(() => {
+    setActiveCode(null);
+    return slickRef.current.slickNext();
+  }, []);
 
   return (
     <div className="eventslides" ref={slideContainerRef}>
@@ -62,19 +86,119 @@ const EventSlide = () => {
               topView="41"
               leftView="20"
               slideContainerRef={slideContainerRef}
+              code={findCode(6009)}
+              productCode={6009}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
             />
             <EventPoint
               topView="64"
               leftView="50"
               slideContainerRef={slideContainerRef}
+              code={findCode(6010)}
+              productCode={6010}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="73"
+              leftView="14"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6006)}
+              productCode={6006}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="76"
+              leftView="69"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6007)}
+              productCode={6007}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
             />
           </div>
         </div>
         <div>
-          <div className="item bg2"></div>
+          <div className="item bg2">
+            <EventPoint
+              topView="25"
+              leftView="37"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6004)}
+              productCode={6004}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="50"
+              leftView="27"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6008)}
+              productCode={6008}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="49"
+              leftView="56"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6005)}
+              productCode={6005}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="56"
+              leftView="46"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6003)}
+              productCode={6003}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="62"
+              leftView="66"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6006)}
+              productCode={6006}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+          </div>
         </div>
         <div>
-          <div className="item bg1"></div>
+          <div className="item bg1">
+            <EventPoint
+              topView="48"
+              leftView="33"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6001)}
+              productCode={6001}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="59"
+              leftView="55"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6002)}
+              productCode={6002}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+            <EventPoint
+              topView="73"
+              leftView="82"
+              slideContainerRef={slideContainerRef}
+              code={findCode(6003)}
+              productCode={6003}
+              activeCode={activeCode}
+              setActiveCode={setActiveCode}
+            />
+          </div>
         </div>
       </Slider>
       <ul className="custom_arrows">
